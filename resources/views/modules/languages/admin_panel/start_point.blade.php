35cf83777f664e4597c14f88ee389c5a4ce06f92
shotaKvitsashvili <kvitsashvili69@gmail.com>
2021-03-19 18:30:49 +0400

    sort.semi.finished

--- resources/views/modules/languages/admin_panel/start_point.blade.php
+++ resources/views/modules/languages/admin_panel/start_point.blade.php
@@ -12,16 +12,14 @@
 	<div class="p-2 module-entry-main">
 		@include('admin.includes.addButton', ['text' => 'Language', 'url' => route('languageAdd')])
 
-		<div class="drag_target_horizontal_div" id="drag_target_0_div">
-			<div></div>
+		<div>
+			@foreach($languages as $data)
+				@include('admin.includes.horizontalEditDeleteBlock', [
+					'title' => $data -> title,
+					'editLink' => route('languageEdit', $data -> id),
+					'deleteLink' => route('languageDelete', $data -> id)
+				])
+			@endforeach
 		</div>
-
-		@foreach($languages as $data)
-			@include('admin.includes.horizontalEditDeleteBlock', [
-				'title' => $data -> title,
-				'editLink' => route('languageEdit', $data -> id),
-				'deleteLink' => route('languageDelete', $data -> id)
-			])
-		@endforeach
 	</div>
 @endsection
