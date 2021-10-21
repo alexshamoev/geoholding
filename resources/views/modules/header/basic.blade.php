184c3acf27ef2f57cd95edcdab468ef7d3cfabfd
Tornike Nizharadze <nijara.g05@gmail.com>
2021-10-21 14:56:07 +0400

    write basic header style and changed row's default style

--- resources/views/modules/header/basic.blade.php
+++ resources/views/modules/header/basic.blade.php
@@ -1,7 +1,7 @@
 @if($widgetGetVisibility['header'])
     <header>
-        <div class="container">
-            <div class="header p-2">
+        <div class="container p-2 mainHeader">
+            <div class="row">
                 <div class="d-flex align-items-center justify-content-between">
                     <div class="p-2">
                         <span>{{ $bsw -> phone_number }}: {{ $bsc -> phone_number }}</span>
@@ -26,34 +26,42 @@
                     </div>
                 </div>
 
-                <div class="d-flex align-items-center">
-                    <div class="col-xl-10 col-lg-10 col-12">
-                        <div class="row align-items-center">
-                            <div class="col-xl-2 col-lg-2 col-12 p-0 d-flex justify-content-between align-items-center">
-                                <div class="p-2">
-                                    <img src="{{ asset('/images/admin/logo.svg') }}" alt="HobbyStudio">
-                                </div>
-
-                                <div class="navbar-toggler d-lg-none d-block" 
-                                    data-toggle="collapse" 
-                                    data-target="#navbar" 
-                                    aria-controls="navbar" 
-                                    aria-expanded="false" 
-                                    aria-label="Toggle navigation">
-                                    <div class="navbar__toggler-icon"></div>
-                                </div>
-                            </div>
-
-                            <div class="col-xl-10 col-lg-10 col-12 p-0">
-                                @include('modules.menu_buttons.basic')
+                <nav class="navbar
+                            col-12
+                            navbar-expand-xl
+                            navbar-light
+                            p-0">
+                    <div class="row d-flex w-100 align-items-center">
+                  
+                        <div class="col-xl-1 col-md-2 col-3 mainHeader__logo order-0">
+                            <div class="p-2">
+                                <img src="{{ asset('/images/admin/logo.svg') }}" alt="HobbyStudio" class="w-100">
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-2 p-0 d-xl-block d-lg-block d-none">
-                        @include('modules.languages.basic')
+                        <div class="order-lg-4 order-3">
+                            <div class="p-0 mainHeader__mobNav">
+                                <button class="navbar-toggler p-0 ms-sm-3 ms-2"
+                                        type="button"
+                                        data-bs-toggle="collapse"
+                                        data-bs-target="#navbar"
+                                        aria-controls="navbar"
+                                        aria-expanded="false" 
+                                        aria-label="Toggle navigation">
+                                    <div class="navbar__toggler-icon"></div>
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-7 col-12 order-xl-1 order-4">
+                            @include('modules.menu_buttons.basic')
+                        </div>
+                        
+                        <div class="col-xl-2 col-6 ms-auto d-block order-lg-2 order-1">
+                            @include('modules.languages.basic')
+                        </div>
                     </div>
-                </div>
+                </nav>
             </div>
         </div>
     </header>
