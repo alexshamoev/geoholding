ecc426893d29495abcf9ffd2f43302870fd5d953
Kubla61 <lasha.kublashvili496@ens.tsu.edu.ge>
2021-11-04 02:37:01 +0400

    Database

--- config/database.php
+++ config/database.php
@@ -42,15 +42,15 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
+ 
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '8889'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel_template'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
