937bc4c9e164734f15416254f035a970b81c4a22
alexshamoev <alexshamoev@gmail.com>
2021-12-06 16:22:44 +0400

    Partners styles

--- resources/views/modules/partners/step0.blade.php
+++ resources/views/modules/partners/step0.blade.php
@@ -1,19 +1,19 @@
 @if($widgetGetVisibility['partners'])
-	<div class="container partners">
+	<div class="container p-2 partners">
 		<h1 class="p-2">
 			Partners widget
 		</h1>
 
 		<div class="row">
 			@foreach($partners as $data)
-				<div class="col-lg-3 col-sm-6 col-12 p-3 partners__partner">
+				<div class="col-xxl-3 col-lg-4 col-md-6 col-12 mb-3 partners__partner">
 					<a href="{{ $data -> link }}" target="_blank">
 						<div class="p-2">
 							<img src="{{ asset('/storage/images/modules/partners/'.$data -> id.'.jpg') }}" alt="{{ $data -> title }}">
 						</div>
 						
-						<div class="px-2 text-center partners__title">
-							<h3>
+						<div class="p-2 text-center partners__title">
+							<h3 class="line_max_3">
 								{{ $data -> title }}
 							</h3>
 						</div>
