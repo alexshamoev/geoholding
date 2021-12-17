dd02274d75b6c5b43047823995689feeae6137e1
alexshamoev <alexshamoev@gmail.com>
2021-12-17 22:44:54 +0400

    A. Css fixes

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -1,13 +1,11 @@
 <footer>
-	<div class="row align-items-center p-2">
-		<div class="col-6">
+	<div class="row p-2">
+		<div class="col-6 p-2">
 			© {{ $copyrightDate }} {{ $bsw -> copyright }}
 		</div>
 		
-		<div class="col-6 justify-content-end d-flex align-items-center">
-			<div>
-				Created by <a href="http://hobbystudio.ge" target="_blank">HobbyStudio</a>
-			</div>
+		<div class="col-6 p-2 d-flex justify-content-end">
+			Created by&nbsp;<a href="http://hobbystudio.ge" target="_blank">HobbyStudio</a>
 		</div>
 	</div>
 </footer>
--- resources/views/admin/includes/horizontalEditDeleteBlock.blade.php
+++ resources/views/admin/includes/horizontalEditDeleteBlock.blade.php
@@ -1,8 +1,8 @@
 <div class="p-2 blockWithRang" data-id="{{ $id }}">
 	<div class="d-flex align-items-center edit-block">
-		<div class="col-10 p-0">
+		<div class="col-10">
 			<div class="row">
-				<div class="col-4">
+				<div class="col-4 p-2">
 					<div class="p-2 d-flex align-items-center">
 						<a href="{{ $editLink }}">
 							<span class="line_max_1">
@@ -12,7 +12,7 @@
 					</div>
 				</div>
 
-				<div class="col-4 d-flex align-items-center">
+				<div class="col-4 p-2 d-flex align-items-center">
 					<div class="p-2 d-flex align-items-center">
 						<span class="line_max_1">
 							@if(isset($text))
@@ -34,24 +34,24 @@
 			</div>
 		</div>
 		
-		<div class="col-2 p-0">
+		<div class="col-2">
 			<div class=" d-flex justify-content-end">
-				<div class="text-right edit-block__edit-delete-block">
+				<div class="edit-block__edit-delete-block p-2">
 					<a href="{{ $editLink }}">
-						<div class="p-3">
+						<div class="p-2">
 							<img src="{{ asset('/storage/images/admin/edit.svg') }}" alt="edit" class="bar-tag-bigger-img">
 						</div>
 					</a>
 				</div>
 
-				<div class="text-right edit-block__edit-delete-block delete-block" data-delete-link="{{ $deleteLink }}">
-					<div class="p-3">
+				<div class="edit-block__edit-delete-block p-2 delete-block" data-delete-link="{{ $deleteLink }}">
+					<div class="p-2">
 						<img src="{{ asset('/storage/images/admin/close.svg') }}" alt="close" class="bar-tag-bigger-img">
 					</div>
 				</div>
 
-				<div class="text-right edit-block__edit-delete-block edit-block__edit-delete-block--move rangButton">
-					<div class="p-3">
+				<div class="edit-block__edit-delete-block p-2 edit-block__edit-delete-block--move rangButton">
+					<div class="p-2">
 						<img src="{{ asset('/storage/images/admin/bars.svg') }}" alt="Move" class="bar-tag-bigger-img">
 					</div>
 				</div>
