d4b4e15f335c88333003743cc5b114aef3619253
Kubla61 <lasha.kublashvili496@ens.tsu.edu.ge>
2022-04-05 18:15:14 +0400

    Core Step 0 Fix up

--- resources/views/modules/core/step0.blade.php
+++ resources/views/modules/core/step0.blade.php
@@ -20,12 +20,17 @@
 				</div>
 			</div>
 		@endif
-
-
-		@include('admin.includes.addButton', [
-			'text' => __('bsw.add').' '.$moduleStep -> title,
-			'url' => route('coreAddStep0', $module -> alias)
-		])
+			@if(!$moduleStep -> images)
+				@include('admin.includes.addButton', [
+					'text' => __('bsw.add').' '.$moduleStep -> title,
+					'url' => route('coreAddStep0', $module -> alias)
+				])
+			@else 
+				{{ Form :: open(array('route' => array('coreAddMultImageForstep0', $module -> alias, $moduleStep -> id), 'files' => true, 'method' => 'post')) }}
+					{{ Form :: file('images[]', ['multiple' => "multiple", 'class' => "form-control", 'accept' => "image/*"]) }}
+					{{ Form :: submit() }}
+				{{ Form :: close() }}
+			@endif
 
 
 		<div class="row" id="rangBlocks" data-db_table="{{ $moduleStep -> db_table }}">
