c3d503d582690edf50bd21550166e3bca94219d6
MuroBakuridze <muro.bakuridze@gmail.com>
2022-06-02 17:58:58 +0400

    change default text type

---
+++ database/migrations/2022_06_02_132709_change_text_type_in_create_news_step_0.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeTextTypeInCreateNewsStep0 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('news_step_0', function (Blueprint $table) {
+            $table -> text('text_ge') -> nullable() -> change();
+			$table -> text('text_en') -> nullable() -> change();
+			$table -> text('text_ru') -> nullable() -> change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('news_step_0', function (Blueprint $table) {
+            $table -> string('text_ge') -> default('');
+			$table -> string('text_en') -> default('');
+			$table -> string('text_ru') -> default('');
+        });
+    }
+}
