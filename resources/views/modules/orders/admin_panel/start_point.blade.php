3bc1b192aa7cf8ac6824566fc72866962cd8ca84
lasha <lashalashka61@gmail.com>
2022-06-09 16:59:54 +0400

    Little fix

--- resources/views/modules/orders/admin_panel/start_point.blade.php
+++ resources/views/modules/orders/admin_panel/start_point.blade.php
@@ -37,7 +37,7 @@
                     <div class="p-2">
                         სრული თანხა: {{ $data -> full_price}} ₾
                     </div>
-                    
+
                     @foreach($data -> orderProducts as $dataInside)
                         <div class="p-2">
                             <span>პროდუქტი: {{ $dataInside -> products -> title }}</span> <br>
@@ -50,30 +50,4 @@
             </div>
         </div>
     @endforeach
-
-	{{-- <div class="p-2">
-        <div class="p-2">
-            <div class="standard-block p-2">
-                <div class="p-2">
-                    ელ. ფოსტის მისამართი: *
-                </div>
-                <p>სახელი: {{ $emailData['user'] -> name}}</p>
-                <p>გვარი: {{ $emailData['user'] -> last_name}}</p>
-                <p>ელ.ფოსტა: {{ $emailData['user'] -> email}}</p>
-                <p>ტელეფონი: {{ $emailData['user'] -> phone}}</p>
-                <p>მისამართი: {{ $emailData['user'] -> address}}</p>
-                <p>შეკვეთის თარიღი: {{ $emailData['order'] -> created_at}}</p>
-                <p>სრული თანხა: {{ $emailData['order'] -> full_price}} ₾</p>
-
-                @foreach($emailData['products'] as $key => $data)
-                <div class="p-2">
-                    <span>პროდუქტი: {{ $data -> { 'title_'.$emailData['language'] } }}</span> <br>
-                    <span>ფასი: {{ $data -> price }} ₾</span> <br>
-                    <span>რაოდენობა: {{ $emailData['quantity'][$key] }} ცალი</span> <br>
-                </div>
-                @endforeach
-
-            </div>
-        </div>
-	</div> --}}
 @endsection
