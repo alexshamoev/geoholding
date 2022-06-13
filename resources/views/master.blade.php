bdb2e687c651a16009e09886493e229496d7bc7c
MuroBakuridze <muro.bakuridze@gmail.com>
2022-06-13 15:42:30 +0400

    Favicon for template

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,6 +13,7 @@
 
 		<link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
 		<link rel="stylesheet" type="text/css" href="{{ asset('css/'.app() -> getLocale().'.css') }}">
+		<link rel="shortcut icon" href="{{ asset('/storage/images/site_icon.png') }}">
 
 		<!-- Plugins -->
 			<!-- photoswipe -->
