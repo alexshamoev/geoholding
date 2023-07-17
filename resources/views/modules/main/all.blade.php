ad5225e297f5f20416b4110776d607a65181519d
Nika_Kvaratskhelia <barca10111@gmail.com>
2023-07-17 15:32:36 +0400

    companies logo

--- resources/views/modules/main/all.blade.php
+++ resources/views/modules/main/all.blade.php
@@ -1,9 +1,18 @@
 
 <div class="d-flex justify-content-around">
 	@foreach ($companies as $item)
-		{{ $item->fullUrl }}
+	
 		<a href="{{ $item->fullUrl }}">
+			@if (file_exists(public_path('storage/images/modules/companies/79/' . $item->id . '.jpg')))
+				<img class="w-25" src="{{ asset('storage/images/modules/companies/79/' . $item->id . '.jpg') }}" alt="{{ $item->title }}" style="background-color: black">
+			@endif
+
+			@if (file_exists(public_path('storage/images/modules/companies/79/' . $item->id . '.svg')))
+				<img class="w-25" src="{{ asset('storage/images/modules/companies/79/' . $item->id . '.svg') }}" alt="{{ $item->title }}" style="background-color: black">
+			@endif
+			
 			<h1>{{ $item->title }}</h1>
 		</a>
+	
 	@endforeach
 </div>
