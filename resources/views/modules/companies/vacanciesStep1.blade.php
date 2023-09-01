763fb72b3ea9008dd3488e26f39e965f5022f6d0
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2023-09-01 16:03:44 +0400

    css classes update

--- resources/views/modules/companies/vacanciesStep1.blade.php
+++ resources/views/modules/companies/vacanciesStep1.blade.php
@@ -10,20 +10,20 @@
 			@isset($active)
 			<div class="col-11 border rounded-5 vacancies_step1__header_wrapper p-0">
 				<div class="row border-bottom border-info p-2">
-					<span class="col-3">{{ __('bsw.position_name') }}:</span>
-					<span class="col-7">{{ $active->title }}</span>
+					<span class="col-lg-3 col-sm-6">{{ __('bsw.position_name') }}:</span>
+					<span class="col-lg-7 col-sm-6">{{ $active->title }}</span>
 				</div>
 				<div class="row border-bottom border-info p-2">
-					<span class="col-3">{{ __('bsw.last_date') }} / {{ __('bsw.published') }}:</span>
-					<span class="col-7">{{ $active->last_date }} / {{ date_format($active->created_at,"d.m.Y") }}</span>
+					<span class="col-lg-3 col-sm-6">{{ __('bsw.last_date') }} / {{ __('bsw.published') }}:</span>
+					<span class="col-lg-7 col-sm-6">{{ $active->last_date }} / {{ date_format($active->created_at,"d.m.Y") }}</span>
 				</div>
 				<div class="row border-bottom border-info p-2">
-					<span class="col-3">{{ __('bsw.location') }}:</span>
-					<span class="col-7">{{ __($active->location) }}</span>
+					<span class="col-lg-3 col-sm-6">{{ __('bsw.location') }}:</span>
+					<span class="col-lg-7 col-sm-6">{{ __($active->location) }}</span>
 				</div>
 				<div class="row p-2">
-					<span class="col-3">{{ __('bsw.send_resume') }}:</span>
-					<span class="col-7">{{ $active->email }}</span>
+					<span class="col-lg-3 col-sm-6">{{ __('bsw.send_resume') }}:</span>
+					<span class="col-lg-7 col-sm-6">{{ $active->email }}</span>
 				</div>
 			</div>
 				<div class="mt-5 col-11">
